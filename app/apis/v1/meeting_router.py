427b474a0b2a48e42775dfb5295ee5b768cd9d76
yoo-sh-96 <tmdguq90051@gmail.com>
2025-07-13 15:21:26 +0900

    date_range dummy api 추가

--- app/apis/v1/meeting_router.py
+++ app/apis/v1/meeting_router.py
@@ -1,8 +1,15 @@
-from fastapi import APIRouter
+from datetime import datetime
+
+from fastapi import APIRouter, HTTPException
+from starlette.status import HTTP_404_NOT_FOUND
 
 from app.dtos.create_meeting_response import CreateMeetingResponse
 from app.dtos.get_meeting_response import GetMeetingResponse
-from app.service.meeting_service_mysql import service_create_meeting_mysql
+from app.dtos.update_meeting_request import UpdateMeetingDateRangeRequest
+from app.service.meeting_service_mysql import (
+    service_create_meeting_mysql,
+    service_get_meeting_mysql,
+)
 
 edgedb_router = APIRouter(prefix="/v1/edgedb/meetings", tags=["Meeting"])
 mysql_router = APIRouter(prefix="/v1/mysql/meetings", tags=["Meeting"])
@@ -12,9 +19,9 @@ mysql_router = APIRouter(prefix="/v1/mysql/meetings", tags=["Meeting"])
 # 실전에서는 db이름을 url에 넣지 말자!
 
 
-@edgedb_router.post("", description="meeting을 생성합니다")
-async def api_create_meeting_edgedb() -> CreateMeetingResponse:
-    return CreateMeetingResponse(url_code="abc")
+# @edgedb_router.post("", description="meeting을 생성합니다")
+# async def api_create_meeting_edgedb() -> CreateMeetingResponse:
+#     return CreateMeetingResponse(url_code="abc")
 
 
 @mysql_router.post("", description="meeting을 생성합니다.")
@@ -27,4 +34,28 @@ async def api_create_meeting_mysql() -> CreateMeetingResponse:
     description="meeting을 조회합니다.",
 )
 async def api_get_meeting_mysql(meeting_url_code: str) -> GetMeetingResponse:
-    return GetMeetingResponse(url_code="abc")
+    meeting = await service_get_meeting_mysql(meeting_url_code)
+    if meeting is None:
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND, detail=f"meeting with url_code: {meeting_url_code} not found"
+        )
+    return GetMeetingResponse(
+        url_code=meeting.url_code,
+        end_date=datetime.now().date(),
+        start_date=datetime.now().date(),
+        title="test",
+        location="test",
+    )
+
+
+@mysql_router.patch("/{meeting_url_code}/date_range", description="meeting 의 날짜 range 를 설정합니다.")
+async def api_update_meeting_date_range_mysql(
+    meeting_url_code: str, update_meeting_date_range_request: UpdateMeetingDateRangeRequest
+) -> GetMeetingResponse:
+    return GetMeetingResponse(
+        url_code="abc",
+        start_date=datetime.now().date(),
+        end_date=datetime.now().date(),
+        title="test",
+        location="test",
+    )
--- app/dtos/get_meeting_response.py
+++ app/dtos/get_meeting_response.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from pydantic import BaseModel
 
 from app.dtos.frozen_config import FROZEN_CONFIG
@@ -5,4 +7,9 @@ from app.dtos.frozen_config import FROZEN_CONFIG
 
 class GetMeetingResponse(BaseModel):
     model_config = FROZEN_CONFIG
+
     url_code: str
+    start_date: date | None = None
+    end_date: date | None = None
+    title: str
+    location: str
